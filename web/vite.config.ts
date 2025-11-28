d6dab72b3ec0b2f468f3e7301649421dd73eeb94
DanielSerio <dserio.d@gmail.com>
2025-11-28 15:05:06 -0500

    feat: Add foundational UI components (shad CN), layout hooks, and project configuration.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,3 +1,5 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
@@ -8,6 +10,12 @@ export default defineConfig({
       target: 'react',
       autoCodeSplitting: true,
     }),
-    react()
+    react(),
+    tailwindcss()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });

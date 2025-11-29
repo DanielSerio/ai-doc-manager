3e3d3db0831a760a3021b688de7be25aad065993
DanielSerio <dserio.d@gmail.com>
2025-11-29 09:15:10 -0500

    feat: Add initial web project setup including Storybook configuration, core assets, and an AsyncButton component.

---
+++ web/src/stories/ui/AsyncButton.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta } from "@storybook/react";
+import { AsyncButton } from "@/components/ui/async-button";
+import { Save } from "lucide-react";
+
+const meta = {
+  component: AsyncButton,
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+  args: {
+    variant: "default",
+    size: "default",
+    children: 'Default Button',
+    icon: <Save />
+  },
+} satisfies Meta<typeof AsyncButton>;
+
+export default meta;
+
+export const Default = {
+  args: {},
+};
+
+export const Loading = {
+  args: {
+    isBusy: true,
+  },
+};
